456cd4a33e74a4f86f4ce0984f11628c9e68027c
troymeeker <troymeeker1@gmail.com>
2022-04-14 12:08:25 -0700

    user tracker

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import usersReducer from "./features/users/usersSlice";
 
 // add imports and code
+const store = createStore(
+  usersReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 ReactDOM.render(
   // add imports and code
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   // add imports and code
   document.getElementById("root")
 );
